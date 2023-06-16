d43ec11191817aec77ebfac516972fc1ef853f9c
Deal(一线灵) <halx99@live.com>
2023-06-17 00:03:44 +0800

    Update build-ios.sh

--- build-ios.sh
+++ build-ios.sh
@@ -1,6 +1,9 @@
 export CPM_SOURCE_CACHE=$HOME/.cache/CPM
 export OPENSSL_ROOT_DIR="/opt/homebrew/opt/openssl@3"
 
+python -V
+pip install jsonschema
+
 rm -rf build/
 mkdir -p build
 cd build && cmake ../ -G Xcode -DCMAKE_TOOLCHAIN_FILE=cmake/ios.toolchain.cmake -DPLATFORM=OS64COMBINED -DENABLE_BITCODE=FALSE && cmake --build . --config Debug
